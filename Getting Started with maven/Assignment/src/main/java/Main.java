528086d9375984b470d0ce0dbb284da899d504d8
Yankeesti <timheinsberg@web.de>
2023-01-10 18:53:18 +0100

    Assignment section 4
    Created Repository and completed Assignment of section 4

---
+++ Getting Started with maven/Assignment/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import org.apache.commons.lang3.*;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.capitalize("hello world"));
+        System.out.println("test");
+    }
+}
